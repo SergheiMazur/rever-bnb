ad60aa85c13388ad6fc085587950bd15dfd8d28d
Elie Harfouche <ehfche@gmail.com>
2018-08-13 16:55:24 -0400

    added booking model 2

--- db/migrate/20180813202534_create_bookings.rb
+++ db/migrate/20180813202534_create_bookings.rb
@@ -2,7 +2,7 @@ class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
       t.references :user
-      t.string :game_id
+      t.references :game
       t.string :status
       t.string :place
 
