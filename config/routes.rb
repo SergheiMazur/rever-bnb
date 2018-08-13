62502de00a77dd82e5f8c8ea6949ce93e89370b5
Elie Harfouche <ehfche@gmail.com>
2018-08-13 17:12:55 -0400

    games controller + views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'games/index'
+  get 'games/show'
+  get 'games/new'
+  get 'games/create'
+  get 'games/edit'
+  get 'games/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
