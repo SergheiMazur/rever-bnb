2d56bbd6088897a575811fdd2d62cffe174dc1e4
Elie Harfouche <ehfche@gmail.com>
2018-08-14 13:25:45 -0400

    bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,42 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def show
+    @booking = booking.find(params[:id])
+    @user = User.new
   end
 
   def new
+    @booking = booking.new
   end
 
   def create
-  end
-
-  def destroy
+    @booking = booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render 'new'
+    end
   end
 
   def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to games_path
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :rating, :description)
   end
 end
