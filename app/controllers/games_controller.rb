a135da698a46d586b802ac241b3c35f4947b7a31
Ali Zahir <alizahir@live.com>
2018-08-14 11:09:40 -0400

    fixed the typo

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,7 +15,7 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(game_params)
     if @game.save
-      redirect_to cocktail_path(@game)
+      redirect_to game_path(@game)
     else
       render 'new'
   end
